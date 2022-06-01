ac37ae8ce73c933f966c749a08f9db45031532b8
atof16 <atofh3x4mo@gmail.com>
2022-06-02 03:33:24 +0900

    worksページにcssを適用

--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -1,11 +1,16 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import styles from './Works.module.css';
 
 export default function Works() {
   return (
     <Layout>
-      <div>
-        <h1>Works</h1>
-        <div>作ったものをここに載せていきます</div>
+      <Head>
+        <title>Works</title>
+      </Head>
+      <div className={styles.worksContainer}>
+        <h2 className={styles.pageTitle}>Works</h2>
+        <div>作ったもの</div>
       </div>
     </Layout>
   );
