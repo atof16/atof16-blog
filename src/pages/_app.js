e855b23123e265f58bf3749df59ab79c3cd64682
atof16 <atofh3x4mo@gmail.com>
2022-06-03 14:04:26 +0900

    コードブロックのシンタックスハイライトを変更

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import 'highlight.js/styles/default.css';
+import 'highlight.js/styles/monokai-sublime.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
