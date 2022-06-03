69aa1043b4f0b3ee798a2e559fafc50b31e45bf1
atof16 <atofh3x4mo@gmail.com>
2022-06-03 20:34:59 +0900

    ヘッダーのレイアウトを変更

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { MdPerson, MdHome, MdOutlineTextSnippet, MdStarBorder } from 'react-icons/md';
 import styles from './header.module.css';
 
 export default function Header() {
@@ -11,20 +12,40 @@ export default function Header() {
               <Link href='/'>
                 <a className={styles.headerItemLink}>Home</a>
               </Link>
+              <Link href='/'>
+                <a>
+                  <MdHome className={styles.headerItemIcon} size={35} />
+                </a>
+              </Link>
             </li>
             <li className={styles.headerItem}>
               <Link href='/blog'>
                 <a className={styles.headerItemLink}>Blog</a>
+              </Link>
+              <Link href='/blog'>
+                <a>
+                  <MdOutlineTextSnippet className={styles.headerItemIcon} size={35} />
+                </a>
               </Link>
             </li>
             <li className={styles.headerItem}>
               <Link href='/profile'>
                 <a className={styles.headerItemLink}>Profile</a>
               </Link>
+              <Link href='/profile'>
+                <a>
+                  <MdPerson className={styles.headerItemIcon} size={35} />
+                </a>
+              </Link>
             </li>
             <li className={styles.headerItem}>
               <Link href='/works'>
                 <a className={styles.headerItemLink}>Works</a>
+              </Link>
+              <Link href='/works'>
+                <a>
+                  <MdStarBorder className={styles.headerItemIcon} size={35} />
+                </a>
               </Link>
             </li>
           </ul>
