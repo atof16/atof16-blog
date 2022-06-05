643ec4f168771add5ad35534f13b1669db4fa8de
atof16 <atofh3x4mo@gmail.com>
2022-06-05 10:35:53 +0900

    トップページを修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Footer from '../components/footer';
+import Layout from '../components/layout';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>Atof16 blog</title>
       </Head>
-      <div className={styles.messages}>
-        <h1 className={styles.helloWorld}>Hello World!</h1>
-        <p className={styles.welcomeMessage}>Welcome to Atof16 home page!</p>
-      </div>
-      <nav>
-        <div className={styles.contents}>
-          <Link href='/blog'>
-            <a className={styles.content}>Blog</a>
-          </Link>
-          <Link href='/profile'>
-            <a className={styles.content}>Profile</a>
-          </Link>
-          <Link href='/works'>
-            <a className={styles.content}>Works</a>
-          </Link>
+      <div className={styles.container}>
+        <div className={styles.messages}>
+          <h1 className={styles.helloWorld}>Hello World!</h1>
+          <p className={styles.welcomeMessage}>Welcome to Atof16 home page!</p>
         </div>
-      </nav>
-      <Footer />
-    </div>
+      </div>
+    </Layout>
   );
 }
